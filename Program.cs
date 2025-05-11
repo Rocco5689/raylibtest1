f1bbdbeeb4621895f8887140b2d9c2a1d6649bc7
Matthew <cavallo5689@gmail.com>
2025-05-10 20:12:56 -0500

    first

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Raylib_cs;
 
 namespace proj1;
 
@@ -6,6 +7,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Initialize window
+        Raylib.InitWindow(800, 600, "2D Square Example");
+        Raylib.SetTargetFPS(60);
+
+        // Main game loop
+        while (!Raylib.WindowShouldClose())
+        {
+            // Begin drawing
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            // Draw a square (rectangle with equal sides)
+            Raylib.DrawRectangle(350, 250, 100, 100, Color.Red);
+
+            // End drawing
+            Raylib.EndDrawing();
+        }
+
+        // Clean up
+        Raylib.CloseWindow();
     }
 }
