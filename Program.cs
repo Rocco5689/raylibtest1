2aa245ad421af209c9e52c85e1b8ad3ebcdd9b03
Matthew <cavallo5689@gmail.com>
2025-05-10 20:17:43 -0500

    first

--- Program.cs
+++ Program.cs
@@ -1,25 +1,38 @@
-﻿using System;
-using Raylib_cs;
-
-namespace proj1;
+﻿using Raylib_cs;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         // Initialize window
-        Raylib.InitWindow(800, 600, "2D Square Example");
+        Raylib.InitWindow(800, 600, "2D Square Movement Example");
         Raylib.SetTargetFPS(60);
+
+        // Square properties
+        float squareX = 350f; // Starting X position
+        float squareY = 250f; // Starting Y position
+        float squareSize = 100f; // Square size
+        float moveSpeed = 5f; // Pixels per frame
 
         // Main game loop
         while (!Raylib.WindowShouldClose())
         {
+            // Update: Handle arrow key input
+            if (Raylib.IsKeyDown(KeyboardKey.Right))
+                squareX += moveSpeed;
+            if (Raylib.IsKeyDown(KeyboardKey.Left))
+                squareX -= moveSpeed;
+            if (Raylib.IsKeyDown(KeyboardKey.Up))
+                squareY -= moveSpeed;
+            if (Raylib.IsKeyDown(KeyboardKey.Down))
+                squareY += moveSpeed;
+
             // Begin drawing
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.White);
+            Raylib.ClearBackground(Color.Black);
 
-            // Draw a square (rectangle with equal sides)
-            Raylib.DrawRectangle(350, 250, 100, 100, Color.Red);
+            // Draw the square at its current position
+            Raylib.DrawRectangle((int)squareX, (int)squareY, (int)squareSize, (int)squareSize, Color.White);
 
             // End drawing
             Raylib.EndDrawing();
